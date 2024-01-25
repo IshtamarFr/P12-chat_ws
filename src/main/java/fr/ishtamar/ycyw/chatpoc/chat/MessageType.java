8abde33a7c3bc7842e5ccbd6d9ce0ff5e9139076
IshtamarFr <ishta@ishtamar.fr>
2024-01-25 11:48:51 +0100

    First commit

---
+++ src/main/java/fr/ishtamar/ycyw/chatpoc/chat/MessageType.java
@@ -0,0 +1,8 @@
+package fr.ishtamar.ycyw.chatpoc.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/test/java/fr/ishtamar/ycyw/chatpoc/ChatPocApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ishtamar.ycyw.chatpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
